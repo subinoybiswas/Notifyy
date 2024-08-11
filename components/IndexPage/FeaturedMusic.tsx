0cdcf0a76a00b4d3715b620e094a529a2ba76993
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-08-11 14:13:15 +0530

    update: spotify opening

--- components/IndexPage/FeaturedMusic.tsx
+++ components/IndexPage/FeaturedMusic.tsx
@@ -7,7 +7,7 @@ const FeaturedMusic = () => {
     const { spotifyObj } = useApp();
     return (
         <View className="flex-1 flex-row flex gap-x-2 min-h-[150px] mx-2 items-center mt-2">
-            <TouchableOpacity className="flex-1 rounded-xl bg-[#36C2CE]/80 h-[100%] flex flex-row items-center  w-[100%] " onPress={() => handlePress(`spotify://album/${spotifyObj.album.id}`)}>
+            <TouchableOpacity className="flex-1 rounded-xl bg-[#36C2CE]/80 h-[100%] flex flex-row items-center  w-[100%] " onPress={() => handlePress(`${spotifyObj.external_urls.spotify}`)}>
                 <Text className="absolute right-1 top-2 font-pbold bg-[#36C2CE] p-2 text-sm rounded-full px-3" >Music</Text>
 
                 <Image source={{ uri: spotifyObj.album.images[0].url }} resizeMode="cover" className="h-20 w-20 m-4 rounded-lg" />
